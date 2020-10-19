dba0df0ec91ed81b7d1af1ac691dfebd11c58356
Josef Raschen <josef@raschen.org>
2020-10-19 20:33:37 +0200

    Remove no longer working sections.

--- install.sh
+++ install.sh
@@ -14,12 +14,3 @@ for i in home/*; do
     fi
 done
 
-QTCREATOR_CONFIG_DIR=".config/QtProject/qtcreator"
-
-if [ ! -d "~/${QTCREATOR_CONFIG_DIR}/styles" ]; then
-    echo "create directory '${QTCREATOR_CONFIG_DIR}/styles'"
-    mkdir -p "${QTCREATOR_CONFIG_DIR}/styles"
-fi
-
-# QtCreator config
-ln -s $(pwd)/home/${QTCREATOR_CONFIG_DIR}/styles/qtcreator-colorscheme.xml ~/${QTCREATOR_CONFIG_DIR}/styles/qtcreator-colorscheme.xml
